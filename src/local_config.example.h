2be088508fb5da1dd1d2ab90dc8c762f83e6c2e8
SGTeam <xsgteamx@gmail.com>
2026-06-03 17:22:40 +0800

    Add local config example

---
+++ src/local_config.example.h
@@ -0,0 +1,6 @@
+#pragma once
+
+const char* WEB_PASSWORD = "CHANGE_ME_WEB_PASSWORD";
+const char* SETUP_AP_SSID = "SG_Duck";
+const char* SETUP_AP_PASS = "CHANGE_ME_12345678";
+const char* DEVICE_NAME = "SG_Duck";
